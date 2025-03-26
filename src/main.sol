7169f2b0ae0ea603c947be158d804a5b362adf2a
VinayakGE <131621039+VinayakGE@users.noreply.github.com>
2025-03-26 09:23:27 +0530

    Update main.sol

--- src/main.sol
+++ src/main.sol
@@ -1,1 +1,14 @@
-Smart Contract File
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract cSankalp {
+    string public message = "Welcome to cSankalp Smart Contract!";
+    
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+}
